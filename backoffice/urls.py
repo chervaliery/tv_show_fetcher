fe9c5a62af16a40f074b282b09dfc63d4494d02d
chervaliery <yoann.chevalier@protonmail.com>
2026-02-04 17:45:02 +0100

    Fix urls

--- backoffice/urls.py
+++ backoffice/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from backoffice.admin import admin_site
+from backoffice.views import home_view
 
 urlpatterns = [
     path('admin/', admin_site.urls),
+    path('', home_view, name='home'),
 ]
